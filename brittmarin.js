8f340faf36e6451ab4b47c6389f25eb835a35b76
brittmarin <britt-marin@hotmail.com>
2015-05-27 14:41:55 +0200

    1
    1

---
+++ brittmarin.js
@@ -0,0 +1,33 @@
+var main = function () {
+  "use strict";
+  
+  	$(".homebutton").on("click", function( e ) {
+    e.preventDefault();
+	$("body, html").animate({ 
+        scrollTop: $( $(this).attr('#home') ).offset().top 
+    }, 600);
+
+
+    $(".aboutbutton").on("click", function( e ) {
+    e.preventDefault();
+	$("body, html").animate({ 
+        scrollTop: $( $(this).attr('#about') ).offset().top 
+    }, 600);
+
+
+    $(".myworkbutton").on("click", function( e ) {
+    e.preventDefault();
+	$("body, html").animate({ 
+        scrollTop: $( $(this).attr('#mywork') ).offset().top 
+    }, 600);
+
+
+    $(".contactbutton").on("click", function( e ) {
+    e.preventDefault();
+	$("body, html").animate({ 
+        scrollTop: $( $(this).attr('#contact') ).offset().top 
+    }, 600);
+    
+});
+
+$("document").ready(main);
